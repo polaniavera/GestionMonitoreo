de7729c0b393d687e13577944476ee19e8ca6e55
polaniavera <polaniavera@gmail.com>
2017-03-17 12:25:30 -0500

    add force json on browser  appStart/WebApiConfig.cs

--- GestionMonitoreo/App_Start/WebApiConfig.cs
+++ GestionMonitoreo/App_Start/WebApiConfig.cs
@@ -10,6 +10,7 @@ namespace GestionMonitoreo
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
