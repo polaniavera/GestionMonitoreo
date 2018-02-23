b6d33100ec670b1ad84c82c8e9685dd13e809cad
Cristhian Polania <polaniavera@gmail.com>
2018-02-23 13:53:15 +1100

    Update RegistroController.cs

--- GestionMonitoreo/Controllers/RegistroController.cs
+++ GestionMonitoreo/Controllers/RegistroController.cs
@@ -47,7 +47,7 @@ namespace GestionMonitoreo.Controllers
         }
 
         // GET api/registro/5
-        [Authorize]
+        //[Authorize]
         public HttpResponseMessage Get(int id)
         {
             var registro = _registroServices.GetRegistroById(id);
