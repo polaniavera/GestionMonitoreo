9fbcf0d2da5fe373ff4827b250e65c59f923a344
Cristhian Polania <polaniavera@gmail.com>
2018-04-13 12:57:58 +1000

    change path

--- GestionMonitoreo/Controllers/RegistroController.cs
+++ GestionMonitoreo/Controllers/RegistroController.cs
@@ -194,7 +194,7 @@ namespace GestionMonitoreo.Controllers
         [Route("file")]
         public HttpResponseMessage GetFile()
         {
-            string path = HttpContext.Current.Server.MapPath("~/" + "download.pdf");
+            string path = Server.MapPath("~/App_Data" + "download.pdf");
             if (!File.Exists(path))
             {
                 return Request.CreateErrorResponse(HttpStatusCode.NotFound, "The file does not exist.");
